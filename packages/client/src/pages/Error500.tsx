2bd44bb6449ce9d5884d7d83c8a09ad0aa333297
Sergei <name.diesel@gmail.com>
2026-05-24 18:43:17 +0300

    TASK-14 Page 500

--- packages/client/src/pages/Error500.tsx
+++ packages/client/src/pages/Error500.tsx
@@ -1,10 +1,12 @@
-import { Helmet } from 'react-helmet'
-import { Header } from '../components/Header'
-import { usePage } from '../hooks/usePage'
-import { PageInitArgs } from '../routes'
+import { Helmet } from 'react-helmet';
+import { Header } from '../components/Header';
+import { usePage } from '../hooks/usePage';
+import { PageInitArgs } from '../routes';
+import { Col, Result, Row } from 'antd';
+import { Link } from 'react-router-dom';
 
 export const Error500 = () => {
-  usePage({ initPage: initError500 })
+  usePage({ initPage: initError500 });
 
   return (
     <div>
@@ -12,12 +14,24 @@ export const Error500 = () => {
         <title>Ошибка 500</title>
       </Helmet>
       <Header />
-      <h1>500</h1>
-      <p>Сервер что-то напутал</p>
+      <Row justify={'center'}>
+        <Col span={8}>
+          <Result
+            status="500"
+            title="500"
+            subTitle="Это не вы, это мы. Честно. Что-то пошло не так на кухне сервера. Уже навели шорох, скоро всё заработает."
+            extra={
+              <Link to="/" type="default">
+                На главную
+              </Link>
+            }
+          />
+        </Col>
+      </Row>
     </div>
-  )
-}
+  );
+};
 
 export const initError500 = async ({ dispatch, state }: PageInitArgs) => {
   // заглушка
-}
+};
