0dc9562f8605afff674c3ebee59707e18aa9490a
Sergei <name.diesel@gmail.com>
2026-05-24 18:34:17 +0300

    TASK-13 Page 404

--- packages/client/src/pages/Error404.tsx
+++ packages/client/src/pages/Error404.tsx
@@ -1,23 +1,37 @@
-import { Helmet } from 'react-helmet'
-import { Header } from '../components/Header'
-import { usePage } from '../hooks/usePage'
-import { PageInitArgs } from '../routes'
+import { Helmet } from 'react-helmet';
+import { Header } from '../components/Header';
+import { usePage } from '../hooks/usePage';
+import { PageInitArgs } from '../routes';
+import { Col, Result, Row } from 'antd';
+import { Link } from 'react-router-dom';
 
 export const Error404 = () => {
-  usePage({ initPage: initError404 })
+  usePage({ initPage: initError404 });
 
   return (
-    <div>
+    <>
       <Helmet>
         <title>Ошибка 404</title>
       </Helmet>
       <Header />
-      <h1>404</h1>
-      <p>Не туда попали</p>
-    </div>
-  )
-}
+      <Row justify={'center'}>
+        <Col span={8}>
+          <Result
+            status="404"
+            title="404"
+            subTitle="Тут ничего нет, даже ошибки. Строго говоря, сейчас должен быть текст про 404, но мы решили, что лучше признаться честно: мы её потеряли."
+            extra={
+              <Link to="/" type="default">
+                На главную
+              </Link>
+            }
+          />
+        </Col>
+      </Row>
+    </>
+  );
+};
 
 export const initError404 = async ({ dispatch, state }: PageInitArgs) => {
   // заглушка
-}
+};
