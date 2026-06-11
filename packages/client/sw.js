96fc7428b7538bcc68a34ba38a4d85fa3c03a2a5
Sergei <name.diesel@gmail.com>
2026-06-11 17:14:30 +0300

    TASK-32 SW

--- packages/client/sw.js
+++ packages/client/sw.js
@@ -3,19 +3,39 @@ const ASSETS_TO_CACHE = [
   '/',
   '/index.html',
   '/assets/index.css',
-  '/assets/index.js'
+  '/assets/index.js',
+  '/biomes/earth/forest/biome.png',
+  '/biomes/earth/forest/sky.png',
+  '/biomes/variation/clouds.webp',
+  '/biomes/variation/desserts.webp',
+  '/biomes/variation/fruits.webp',
+  '/biomes/variation/mountains.webp',
+  '/biomes/variation/sky-v4.webp',
+  '/biomes/variation/water.jpg'
 ]
 
 // 1. Install Event: Cache essential files
 self.addEventListener('install', (event) => {
   console.log('install')
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log('Caching core assets')
+    caches.open(CACHE_NAME).then(async(cache) => {
+      try {
+        // Cache the root assets (static assets like HTML)
+        await cache.addAll(ASSETS_TO_CACHE)
 
-      return cache.addAll(ASSETS_TO_CACHE)
+        // Dynamically cache all CSS files
+        const cssFiles = await fetchAssets('css') // This is a custom function
+        await cache.addAll(cssFiles)
+
+        // Dynamically cache all JS files
+        const jsFiles = await fetchAssets('js') // This is a custom function
+        await cache.addAll(jsFiles)
+      } catch ( error ) {
+        console.error('Failed to cache resources:', error)
+      }
     })
   )
+  self.skipWaiting()
 })
 
 // 2. Activate Event: Clean up old caches
@@ -38,15 +58,14 @@ self.addEventListener('activate', (event) => {
 
 // 3. Fetch Event: Network-first fallback to cache strategy
 self.addEventListener('fetch', (event) => {
+  console.log('fetch')
   event.respondWith(
     fetch(event.request)
       .catch(() => {
         // If network fails, try matching the asset in the cache
-
         return caches.match(event.request).then((response) => {
-          
           // If asset is found in cache, return it; otherwise return offline page
-          return response || caches.match('/offline.html')
+          return response || caches.match('/index.html')
         })
       })
   )
@@ -70,6 +89,7 @@ async function fetchAssets(type) {
 
   let match
   while ( (match = regex.exec(text)) ) {
+    console.log('-', match[1])
     assets.push(match[1]) // Add matched asset URLs
   }
 
--- packages/client/vite.config.ts
+++ packages/client/vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import dotenv from 'dotenv';
 import path from 'path';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 dotenv.config();
 
 // https://vitejs.dev/config/
@@ -19,5 +20,15 @@ export default defineConfig({
   ssr: {
     noExternal: ['react-helmet-async'],
   },
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: './sw.js',
+          dest: '',
+        },
+      ],
+    }),
+  ],
 });
