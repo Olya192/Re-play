829d848e26bfd43456aef260528d0de970c84b1e
Olya192 <olya192@bk.ru>
2026-07-15 21:03:33 +0400

    промежуточный

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -4,20 +4,17 @@ import express from 'express';
 import session from 'express-session';
 import { createClientAndConnect } from './db';
 import { authMiddleware } from './middleware/auth';
+import cookieParser from 'cookie-parser';
 
 dotenv.config();
 
-import { sequelize } from './db';
-import { User } from './models/User';
-import { SiteTheme } from './models/SiteTheme';
-import { UserTheme } from './models/UserTheme';
-import { themeRoutes } from './routes/themeRoutes';
-
-sequelize.addModels([User, SiteTheme, UserTheme]);
+import { startApp } from './db/startApp';
+import router from './router/router';
+import { notFound } from './middleware/notFound';
+import * as console from 'console';
+import { themeRoutes } from './router/themeRoutes';
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
 const port = Number(process.env.SERVER_PORT) || 3001;
 
@@ -29,6 +26,7 @@ app.use(
 );
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use(
   session({
@@ -53,8 +51,7 @@ app.get('/health', (_, res) => {
 
 (async () => {
   try {
-    await sequelize.sync({ alter: true });
-    console.log('  ➜ 🎸 Database synchronized');
+    console.log('  ➜ 🎸 Database ready');
 
     themeRoutes(app);
 
@@ -203,14 +200,23 @@ app.get('/health', (_, res) => {
     });
 
     app.get('/', (_, res) => {
-      res.json(' Howdy from the server :)');
+      res.json('👋 Howdy from the server :)');
     });
+
+    app
+      .disable('x-powered-by')
+      .enable('trust proxy')
+      .set('query parser', 'extended')
+      .use(cookieParser())
+      .use(router)
+      .use(notFound);
+
+    await startApp();
 
     app.listen(port, () => {
       console.log(`  ➜ 🎸 Server is listening on port: ${port}`);
     });
   } catch (error) {
-    console.error(' Failed to start server:', error);
-    process.exit(1);
+    console.error('Error starting server:', error);
   }
 })();
